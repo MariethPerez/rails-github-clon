f26ccf89d8134417bbbda96c7240cd874f086717
Diego Torres <diegotc86@gmail.com>
2019-04-04 17:19:10 -0500

    Add POST create: test & controller method

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,6 +8,17 @@ class RepositoriesController < ApplicationController
     render json: Repository.find(params[:id])
     end
 
+  def create
+    repository = Repository.create(
+      name: params[:name], 
+      description: params[:description], 
+      access: params[:access], 
+      license: params[:license], 
+      user_id: params[:user_id],
+      )
+    render json: repository, status: :created
+  end
+
   rescue_from ActiveRecord::RecordNotFound do |e|
     render json: { message: e.message }, status: :not_found
   end
--- spec/controllers/repositories_controller_spec.rb
+++ spec/controllers/repositories_controller_spec.rb
@@ -82,4 +82,54 @@ describe RepositoriesController do
       expect(response).to have_http_status(:not_found)
     end
   end
+
+  describe "POST create" do
+    it "returns http status created" do
+      user = User.create(
+        username: "diegotc86",
+        name: "Diego Torres",
+        birthday: "07/06/1986",
+        email: "diegot86@gmail.com",
+        bio: "Diego's bio here",
+        company: "Diego's company",
+        location: "LIMA - PERU",
+        website: "www.diegotorres.dev"
+        )
+      
+      post :create, params: {
+        name: 'repo1', 
+        description: 'Description of repo1', 
+        access: "public", 
+        license: "none", 
+        user_id: user.id,
+      }
+      expect(response.status).to eq(201)
+      expect(response).to have_http_status(:created)
+    end
+
+    it "returns the created repository" do
+      user = User.create(
+        username: "diegotc86",
+        name: "Diego Torres",
+        birthday: "07/06/1986",
+        email: "diegot86@gmail.com",
+        bio: "Diego's bio here",
+        company: "Diego's company",
+        location: "LIMA - PERU",
+        website: "www.diegotorres.dev"
+        )
+      
+      post :create, params: {
+        name: 'repo1', 
+        description: 'Description of repo1', 
+        access: "public", 
+        license: "none", 
+        user_id: user.id,
+      }
+      expected_repository = JSON.parse(response.body)
+      expect(expected_repository).to have_key("id")
+      expect(expected_repository["name"]).to eq("repo1")
+    end
+  end
+
 end

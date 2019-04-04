1093b353ec6e9c87d88ea17879676b069ae76772
Diego Torres <diegotc86@gmail.com>
2019-04-04 17:03:17 -0500

    Add GET show: test & controller method

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,4 +4,12 @@ class RepositoriesController < ApplicationController
     render json: Repository.all
   end
 
+  def show
+    render json: Repository.find(params[:id])
+    end
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { message: e.message }, status: :not_found
+  end
+
 end
--- spec/controllers/repositories_controller_spec.rb
+++ spec/controllers/repositories_controller_spec.rb
@@ -31,5 +31,55 @@ describe RepositoriesController do
     end
   end
 
-
+  describe 'GET show' do
+    it 'returns http status ok' do
+      user = User.create(
+      username: "diegotc86",
+      name: "Diego Torres",
+      birthday: "07/06/1986",
+      email: "diegot86@gmail.com",
+      bio: "Diego's bio here",
+      company: "Diego's company",
+      location: "LIMA - PERU",
+      website: "www.diegotorres.dev"
+      )
+      repository = Repository.create(
+      name: 'repo1', 
+      description: 'Description of repo1', 
+      access: "public", 
+      license: "none", 
+      user_id: user.id,
+      )
+      get :show, params: { id: repository }
+      expect(response).to have_http_status(:ok)
+    end
+    
+    it 'render the correct repository' do
+      user = User.create(
+      username: "diegotc86",
+      name: "Diego Torres",
+      birthday: "07/06/1986",
+      email: "diegot86@gmail.com",
+      bio: "Diego's bio here",
+      company: "Diego's company",
+      location: "LIMA - PERU",
+      website: "www.diegotorres.dev"
+      )
+      repository = Repository.create(
+      name: 'repo1', 
+      description: 'Description of repo1', 
+      access: "public", 
+      license: "none", 
+      user_id: user.id,
+      )
+      get :show, params: { id: repository }
+      expected_repository = JSON.parse(response.body)
+      expect(expected_repository["id"]).to eq(repository.id)
+    end
+    
+    it 'returns http status not found' do
+      get :show, params: { id: 'xxx' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
 end

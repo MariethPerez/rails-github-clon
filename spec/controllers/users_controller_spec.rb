3241c81f165fb043d53ccea1969161bc8a8e236f
Jona <jcmendozar10@gmail.com>
2019-04-04 17:02:37 -0500

    Git index created

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,36 @@
 require 'rails_helper'
+ 
+describe UsersController do
+  before do
+    User.delete_all
+  end
 
-describe UserController do
+  describe 'GET index' do
+    it 'returns http status ok' do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  it 'render json with all users' do
+    User.create(
+      username: 'Cuevita',
+      name: 'Pedro',
+      birthday: '10/11/1990',
+      email: 'cuevitasmalo666@gmail.com',
+      bio: 'Es muy malo malo jugando pelota',
+      company: 'Maldad Pura',
+      location: 'Los olvidados de Dios',
+      website: 'nodebojugarfutbol@cojo.pe' 
+      )
+    get :index
+    users = JSON.parse(response.body)
+    expect(users.size).to eq 1
+  end
+
+  
 end
+
+  
+
+  

5198b4381a88fa89508a34df995c644f0c319108
paulotijero <paulotijero@gmail.com>
2019-04-05 16:05:54 -0500

    Fix class name

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -1,4 +1,4 @@
 class PullRequest < ApplicationRecord
-  belongs_to :branch_in, class_name: :user
-  belongs_to :branch_out, class_name: :user
+  belongs_to :branch_in, class_name: 'User'
+  belongs_to :branch_out, class_name: 'User'
 end

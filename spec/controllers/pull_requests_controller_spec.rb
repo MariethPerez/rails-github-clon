651ce1e29835b8d0e19c3d224026bc77e3f7f923
paulotijero <paulotijero@gmail.com>
2019-04-05 16:35:52 -0500

    Add index test to pull_request

--- spec/controllers/pull_requests_controller_spec.rb
+++ spec/controllers/pull_requests_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-describe Pull_RequestsController do 
+describe PullRequestsController do 
   
+  describe 'GET index' do
+    it 'returns http status ok' do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
 end

399ecc297eaa78ed7acb393398cf07ced14cd94f
Diego Torres <diegotc86@gmail.com>
2019-04-05 16:25:38 -0500

    Add seeds.rb file using faker gem

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,12 +1,11 @@
 class RepositoriesController < ApplicationController
-
   def index
     render json: Repository.all
   end
 
   def show
     render json: Repository.find(params[:id])
-    end
+  end
 
   def create
     repository = Repository.create(
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Faker::Config.locale = 'es'
+user_id = 1
+repository_id=1
+access = ["public","private"]
+
+50.times do
+  name = Faker::Name.unique.name
+
+  user = {
+    username: Faker::Internet.username(name[0..10]),
+    name: name,
+    birthday: Faker::Date.between(80.year.ago, 10.year.ago),
+    email: Faker::Internet.safe_email(name[0..10]),
+    bio: Faker::Lorem.paragraph,
+    company: Faker::Company.name,
+    location: Faker::Nation.nationality,
+    website: Faker::Internet.domain_name 
+  }
+  User.create(user)
+
+  repo_count = [1,2,3].sample
+  repo_count.times do
+    repository = {
+      name: Faker::App.name, 
+      description: "Description of repository #{repository_id}", 
+      access: access.sample, 
+      license: "none", 
+      user_id: user_id,
+    }
+    Repository.create(repository)
+
+    branch_count = [1,2,3,4,5].sample
+    branch_count.times do
+      branch ={
+        name: Faker::App.name,
+        user_id: user_id,
+        repository_id: repository_id
+      }
+      Branch.create(branch)
+    end
+    repository_id += 1
+  end
+  user_id += 1
+end

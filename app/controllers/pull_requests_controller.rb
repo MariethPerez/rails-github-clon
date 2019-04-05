e5854330dedafe1f3791528bd722ed9a0aaee48c
paulotijero <paulotijero@gmail.com>
2019-04-05 16:33:46 -0500

    Add controllers pull_request

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,3 +1,37 @@
-class Pull_RequestsController < ApplicationController
+class PullRequestsController < ApplicationController
+
+  def index
+    render json: PullRequest.all
+  end
+
+  def show
+    render json: PullRequest.find(params[:id])
+  end
+
+  def create
+    pull_request = PullRequest.create(title: params[:title],description: params[:description],label: params[:label],user_id: params[:user_id])
+    render json: pull_request, status: :created
+  end
+
+  def update
+    pull_request = PullRequest.find(params[:id])
+    params.keys.each do |key|
+      if key != :id && pull_request.attributes.key?(key)
+        pull_request[key] = params[key]
+      end
+    end
+    pull_request.save
+    render json: pull_request
+  end
+
+  def destroy
+    pull_request = PullRequest.find(params[:id])
+    pull_request.destroy
+    render nothing: true, status: :no_content
+  end
+  
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { message: e.message }, status: :not_found
+  end
   
 end

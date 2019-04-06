62f35020ce9a30f15098a94efd86ab1f9fcf0657
Diego Torres <diegotc86@gmail.com>
2019-04-05 19:15:01 -0500

    Add seed data to issues table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 require 'faker'
-
 Faker::Config.locale = 'es'
-user_id = 1
-repository_id=1
-access = ["public","private"]
 
+user_id = 1
+repository_id = 1
+
+#Create 50 users
 50.times do
   name = Faker::Name.unique.name
 
@@ -13,24 +13,26 @@ access = ["public","private"]
     name: name,
     birthday: Faker::Date.between(80.year.ago, 10.year.ago),
     email: Faker::Internet.safe_email(name[0..10]),
-    bio: Faker::Lorem.paragraph,
+    bio: "This is the Bio of #{name}",
     company: Faker::Company.name,
     location: Faker::Nation.nationality,
     website: Faker::Internet.domain_name 
   }
   User.create(user)
 
+  #For each user creates in between 1 to 3 repositories
   repo_count = [1,2,3].sample
   repo_count.times do
     repository = {
       name: Faker::App.name, 
       description: "Description of repository #{repository_id}", 
-      access: access.sample, 
+      access: ["public","private"].sample, 
       license: "none", 
       user_id: user_id,
     }
     Repository.create(repository)
 
+    #For each repository creates in between 1 to 5 branches
     branch_count = [1,2,3,4,5].sample
     branch_count.times do
       branch ={
@@ -43,4 +45,19 @@ access = ["public","private"]
     repository_id += 1
   end
   user_id += 1
+end
+
+# Creates 100 random issues
+issue_id = 1
+100.times do
+  issue ={
+    title: "Tittle of Issue #{issue_id}",
+    description: "Description of Issue #{issue_id}",
+    label: ["none", "bug", "enhancement"].sample,
+    user_create_id: rand(1..(user_id-1)),
+    user_assign_id: rand(1..(user_id-1)),
+    repository_id: rand(1..(repository_id-1))
+  }
+  Issue.create(issue)
+  issue_id += 1
 end
